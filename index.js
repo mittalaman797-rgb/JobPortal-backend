a0bdff8527de495f0b4a637832bd80b04393684d
Aman Mittal <mittalaman797@gmail.com>
2026-02-05 14:55:17 +0530

    first commit

--- index.js
+++ index.js
@@ -17,13 +17,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 const corsOptions = {
-     origin: "*",
+     origin: "https://jobportal-frontend-34.onrender.com/",
      credentials: true
 }
 app.use(cors(
-     {
-          origin:"*"
-     }
+     corsOptions
 ))
     
 
