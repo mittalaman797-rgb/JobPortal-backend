baf46e0f186f2117568a19dc6b42b7dc4cc8e305
Aman Mittal <mittalaman797@gmail.com>
2026-02-06 13:01:37 +0530

    ok

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ dotenv.config()
 
 const app = express()
 // ["https://jobportal-frontend-34.onrender.com",
+const url="https://amazing-capybara-c5c6d9.netlify.app"
+const url2="http://localhost:5173";
 const corsOptions = {
-     origin: "https://jobportal-frontend-34.onrender.com",
+     origin: url,
      credentials: true
 }
 app.use(cors(
